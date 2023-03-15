ca88f48ca7e8431c597a6a797d464f9d9e68e810
RA3PKJ-PC\RA3PKJ <ra3pkj@gmail.com>
2023-03-16 00:11:25 +0300

    recompil (remove dublicate)

--- compilation_pyd_4_2_17__32/python38_32/PC/pyconfig.h
+++ compilation_pyd_4_2_17__32/python38_32/PC/pyconfig.h
@@ -65,7 +65,7 @@ WIN32 is still required for the locale module.
 #define LONG_BIT        32
 #define WORD_BIT 32
 
-#define MS_WIN32 /* only support win32 and greater. */
+//#define MS_WIN32 /* only support win32 and greater. */
 #define MS_WINDOWS
 #ifndef PYTHONPATH
 #       define PYTHONPATH L".\\DLLs;.\\lib"
